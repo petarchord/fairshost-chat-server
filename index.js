f9794a014f619d847d6c6ffa6136441a1115beb5
petarchord <petarchord@hotmail.com>
2021-09-19 14:35:43 +0200

    added chat server implementation

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const https = require("https");
+const socket = require("socket.io");
+const {
+  userJoin,
+  getUserById,
+  userLeave,
+  getRoomUsers,
+  getUsers,
+} = require("./utils/users");
+
+https.globalAgent.options.rejectUnauthorized = false;
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+const PORT = 3001;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionSuccessStatus: 200,
+  credentials: true,
+};
+
+const app = express();
+app.use(cors(corsOptions));
+
+const options = {
+  key: fs.readFileSync("keys/privatekey.pem"),
+  cert: fs.readFileSync("keys/certificate.pem"),
+};
+
+const server = https.createServer(options, app);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("new WS connection with socketid:", socket.id);
+
+  socket.on("joinStreamer", ({ room, username }) => {
+    console.log("users before join:", getUsers());
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "Chat Bot",
+      chatMessage: `${user.username} has joined the chat.`,
+    });
+
+    console.log("roomUsers:", getRoomUsers(user.room));
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on("chatMessage", (message) => {
+    const user = getUserById(socket.id);
+    console.log("message", JSON.stringify(message));
+
+    io.to(user.room).emit("message", message);
+  });
+
+  socket.on("broadcast-resumed", () => {
+    console.log("broadcast-resumed message received");
+    const user = getUserById(socket.id);
+    socket.broadcast.to(user.room).emit("reconnect");
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`client with socket id: ${socket.id} disconnected`);
+    const user = userLeave(socket.id);
+    if (user) {
+      console.log("users after userLeave:", getUsers());
+      io.to(user.room).emit("message", {
+        user: "Chat Bot",
+        chatMessage: `${user.username} has left the chat.`,
+      });
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is up and running at port ${PORT}`);
+});
---
+++ utils/users.js
@@ -0,0 +1,43 @@
+const users = [];
+
+function checkIfUserExists(username, room) {
+  return users.find((user) => user.username === username && user.room === room);
+}
+
+function userJoin(id, username, room) {
+  const user = {
+    id,
+    username,
+    room,
+  };
+  users.push(user);
+
+  return user;
+}
+
+function getUserById(id) {
+  return users.find((user) => user.id === id);
+}
+
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+function getRoomUsers(room) {
+  return users.filter((user) => user.room === room);
+}
+
+function getUsers() {
+  return users;
+}
+
+module.exports = {
+  userJoin,
+  getUserById,
+  userLeave,
+  getRoomUsers,
+  getUsers,
+};
