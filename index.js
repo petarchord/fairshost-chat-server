5bfd8a57eec690786c5cfa3999bcae8b8db51ca3
petarchord <petarchord@hotmail.com>
2021-10-10 01:31:50 +0200

    CORS updated v2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const {
 const port = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://fairshost-fe.herokuapp.com/",
   optionSuccessStatus: 200,
   credentials: true,
 };
